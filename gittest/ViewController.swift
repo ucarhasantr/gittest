259ef47e57a1ad4deb55a60c9f47a8a23319def9
Hasan UCAR <>
2019-01-16 16:19:55 +0300

    first commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by Hasan UCAR on 16.01.2019.
+//  Copyright © 2019 Hasan UCAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("abcdd")
+    
+    }
+
+}
+
